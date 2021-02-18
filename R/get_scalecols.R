c2bfbb2bd8a99d858489cf003d2b743049cab613
amacfarland <andrew.macfarland@colorado.edu>
2021-02-18 13:17:50 -0700

    document get_scalecols

--- R/get_scalecols.R
+++ R/get_scalecols.R
@@ -1,5 +1,26 @@
+#' Get scale columns
+#'
+#' Select all columns with a given prefix from a dataframe.
+#'
+#' @param data a dataframe
+#' @param scale_prefix a character string
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' df <-
+#'   data.frame(
+#'     scale1_foo = sample(1:4, 10, replace = TRUE),
+#'     scale1_bar = sample(1:4, 10, replace = TRUE),
+#'     scale2_foo = sample(1:4, 10, replace = TRUE),
+#'     scale2_bar = sample(1:4, 10, replace = TRUE)
+#' )
+#'
+#' get_scalecols(df, "scale1")
+#'
 get_scalecols <-
   function(data, scale_prefix) {
     data %>%
-      select(., starts_with(scale_prefix))
+      dplyr::select(., tidyselect::starts_with(scale_prefix))
   }
